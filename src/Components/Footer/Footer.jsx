94e89f5f779d2fe058d5eb0d176e8d8485a8235d
Adithyan <adi6239100@gmail.com>
2024-10-07 21:39:09 +0530

    change

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,58 +1,72 @@
-import React from 'react'
-import { FaFacebook,FaInstagram,FaLinkedin } from 'react-icons/fa'
+import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-const footerlink=[
-    {
-        title:"Home",
-        link:"/#",
-    },
-    {
-        title:"About",
-        link:"/#about",
-    },
-    {
-        title:"Contact",
-        link:"/#contact",
-    },
-    {
-        title:"Blog",
-        link:"/#blog",
-    },
-
-]
+const footerlink = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
 
 const Footer = () => {
   return (
-   <>
-   <div>
-    <div className='flex'>
-    <div className='grid grid-cols-2'>
-        <div className='justify-center items-center p-6'>
-    <h3 className='text-2xl font-cursive font-bold text-primary'>Contact Us</h3>
-    </div>
-    <div>
-        <form>
-            <div className='flex gap-5'>
-            <div className=''>
-            <label htmlFor="" className='p-4'>Name</label>
-            <input type="text" className='box-content w-[16rem] h-4 p-2' name='name' />
-            </div>
-            <div className=''>
-            <label htmlFor="" className='p-4'>Email</label>
-            <input type="email" className='box-content box-border border-black-5 w-[16rem] h-4 p-2'/><br />
-            </div>
+    <>
+      <div className="bg-primary">
+        <div className="flex">
+          <div className="grid grid-cols-2">
+            <div className="justify-center items-center p-6">
+              <h3 className="text-3xl font-cursive text-white">Contact Us</h3>
+              <div>
+                
+              </div>
             </div>
             <div>
-            <label htmlFor="" className='p-4'>How can we help you?</label><br />
-            <input type="textarea" className='box-content w-[20rem] h-10 p-2'/>
+              <form>
+                <div className="flex gap-5 p-5">
+                  <div className="">
+                    <label htmlFor="" className="text-white">
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      className="w-[16rem] h-8 p-2"
+                      name="name"
+                    />
+                  </div>
+                  <div className="">
+                    <label htmlFor="" className="p-4 text-white">
+                      Email
+                    </label>
+                    <input type="email" className="w-[16rem] h-8 p-2" />
+                    <br />
+                  </div>
+                </div>
+                <div className="p-4">
+                  <label htmlFor="" className="p-4 text-white">
+                    Phone
+                  </label>
+                  <br />
+                  <input type="number" className=" w-[16rem] h-8 p-2" />
+                </div>
+              </form>
             </div>
-        </form>
-    </div>
-    </div>
-    </div>
-   </div>
-   </>
-  )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
